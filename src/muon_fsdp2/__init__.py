20b1bb50c32b82124616efc326d41d892b2b730d
Sami Jaghouar <sami.jaghouar@hotmail.fr>
2025-06-06 17:51:54 +0000

    bump version

--- src/muon_fsdp2/__init__.py
+++ src/muon_fsdp2/__init__.py
@@ -14,7 +14,7 @@ from torch.distributed import gather, scatter
 import torch.distributed as dist
 from torch import Tensor
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 __all__ = ["Muon", "MuonDDP"]
 
