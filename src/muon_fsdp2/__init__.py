e160a6d91fd178361a61021d94ea6274cda5e099
sami jaghouar <sami.jaghouar@gmail.com>
2025-08-08 12:36:23 +0000

    make pypi project simpler

--- src/muon_fsdp2/__init__.py
+++
@@ -1,5 +0,0 @@
-# Re-export everything from the nested module
-from muon_fsdp2.muon_fsdp2 import __version__, Muon
-
-# Make sure all exports are available at the top level
-__all__ = ["__version__", "Muon"]
